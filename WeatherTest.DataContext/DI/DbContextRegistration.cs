7f9d30e8922d2788fd29caf003019dc85c965f43
Константин Алексеев <arrogant93@mail.ru>
2020-03-19 18:24:25 +0300

    Added registration DbContext

---
+++ WeatherTest.DataContext/DI/DbContextRegistration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Z.EntityFramework.Extensions;
+
+namespace WeatherTest.DataContext.DI
+{
+    public static class DbContextRegistration
+    {
+        private const string DATABASE_KEY = "WeatherTest";
+
+        public static IServiceCollection AddDbContextServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString(DATABASE_KEY);
+
+            services.AddDbContextPool<WeatherTestDbContext>((provider, options) =>
+            {
+                options.UseMySql(connectionString);
+            });
+
+            services.AddDbContext<WeatherTestDbContext>();
+
+            EntityFrameworkManager.ContextFactory = context =>
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<WeatherTestDbContext>();
+                optionsBuilder.UseMySql(connectionString);
+                return new WeatherTestDbContext(optionsBuilder.Options);
+            };
+
+            return services;
+        }
+    }
+}
